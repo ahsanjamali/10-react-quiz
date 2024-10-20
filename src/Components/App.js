970b4ec27dc0f4d24c0510aeb78f1d76583c5884
ahsanjamali <ahsanjamali7042@gmail.com>
2024-10-20 16:04:37 +0500

    Final Change

--- src/Components/App.js
+++ src/Components/App.js
@@ -7,7 +7,7 @@ import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
-import FinishScreen from "../FinishScreen";
+import FinishScreen from "./FinishScreen";
 import Footer from "./Footer";
 import Timer from "./Timer";
 
